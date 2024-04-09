3f07a90f6082991e9c05f0476a6d213999b09015
Tushar4059x <gulyanitushar@gmail.com>
2024-04-09 22:46:18 +0530

    file-structure

---
+++ woods-indica/src/app/components/Landing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from '../Nav/page'
+import Header from '../Header/page'
+export default function Landing() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+    </div>
+  )
+}
